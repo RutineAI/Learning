5e6547499fa8d432e67e805e3908a1748f1445dc
RutineAI <100998619+RutineAI@users.noreply.github.com>
2023-03-15 20:38:43 +0100

    a

--- public_html/app.py
+++ public_html/app.py
@@ -37,5 +37,7 @@ def get_tasks():
     tasks_list = [{"id": task.id, "description": task.description, "bike_details": task.bike_details, "cost": task.cost} for task in tasks]
     return jsonify(tasks_list)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, host="0.0.0.0", port=5001)
