1109880233d284ea82bb696c326384058f831156
RutineAI <100998619+RutineAI@users.noreply.github.com>
2023-03-15 19:58:30 +0100

    g

--- public_html/app.py
+++ public_html/app.py
@@ -32,4 +32,5 @@ def get_tasks():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=False, host="0.0.0.0", port=5001)
+
